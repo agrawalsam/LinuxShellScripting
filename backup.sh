760b59969792bf23f35c56de869bb5bf09753e7f
agrawalsam <31516561+agrawalsam@users.noreply.github.com>
2025-03-07 16:07:27 +0530

    Updated backup.sh

--- backup.sh
+++ backup.sh
@@ -5,10 +5,85 @@ compression=""
 s3_location=""
 archived_file_name=""
 
-log_file='/var/log/user_management.log'
+log_file="${HOME}/backup.log"
 
 log_action(){
     echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" | sudo tee -a $log_file >/dev/null
+}
+
+directory_check(){
+    if [ -z "$1" ]; then 
+        echo "Directory Argument was not provided."
+        log_action "Directory Argument was not provided."
+        exit 1
+    fi
+
+    # check whether a directory exists or not and then check whether it is valid or not. 
+    if ! [ -d "$1" ] || ! [ -r "$1" ]; then 
+        echo "Directory $1 doesnot exists or Directory $1 is not readable."
+        log_action "Directory $1 doesnot exists or Directory $1 is not readable."
+        exit 1
+    fi
+
+    # writes action into log. 
+    log_action "Directory $1 exists and is readable."
+}
+
+compression_check(){
+    if [ -z "$1" ]; then 
+        echo "Compression Argument was not provided."
+        log_action "Compression Argument was not provided."
+        exit 1
+    fi
+
+    # check whether the compress mode is either zip or tar. 
+    if ! [ $1 == "zip" ] && ! [ $1 == "tar" ]; then 
+        echo "$1 is not zip and tar"
+        log_action "$1 is not zip and tar"
+        exit 1
+    fi 
+}
+
+compression(){
+    read -p "Enter the directory where you want the compressed backup to be stored :" input 
+
+    # check whether the output directory exists or not. 
+    directory_check $input
+
+    timestamp=$(date '+%Y-%m-%d_%H-%M-%S')
+    archived_file_name="backup-${timestamp}-$1.$2"
+    archived_file_name="$input/${archived_file_name}"
+    
+    if [ $2 == "zip" ]; then 
+        # using zip command to zip directory
+        zip -r ${archived_file_name} $1
+    elif [ $2 == "tar" ]; then 
+        # using tar command to tar directory
+        tar cf ${archived_file_name} $1
+    fi 
+
+    if [[ $? -ne 0 ]]; then 
+        log_action "Compression Failed for directory $1"
+        exit 1
+    fi         
+
+    # writes action into log. 
+    log_action "Compression for directory $1 Successful: ${archived_file_name}"
+}
+
+
+s3_move() {
+    # Check the given S3 location should be non empty 
+    # Check the given S3 location should start with s3://
+    if [ -n "$s3_location" ] && echo $s3_location | grep -q "^s3://" ; then 
+        log_action "S3 Location $s3_location is provided in the arguments."
+    
+        # Now copy the archived file into S3. 
+        aws s3 cp ${archived_file_name} ${s3_location}
+
+        # after successful copy, we can delete the backup file in server. 
+        rm ${archived_file_name}
+    fi
 }
 
 usage(){
@@ -42,56 +117,10 @@ while getopts "d:c:s:" opt; do
     esac
 done 
 
-if [ -z "$directory" ]; then 
-    echo "Directory Argument was not provided."
-    exit 1
-fi
+directory_check $directory
 
-if [ -z "$compression" ]; then 
-    echo "Compression Argument was not provided."
-    exit 1
-fi
+compression_check $compression
 
-# check whether a directory exists or not and then check whether it is valid or not. 
-if ! [ -d "$directory" ] || ! [ -r "$directory" ]; then 
-    echo "Directory $directory doesnot exists or Directory $directory is not readable."
-    log_action "Directory $directory doesnot exists or Directory $directory is not readable."
-    exit 1
-fi
+compression $directory $compression
 
-# writes action into log. 
-log_action "Directory $directory exists and is readable."
-
-# check whether the compress mode is either zip or tar. 
-if ! [ $compression == "zip" ] && ! [ $compression == "tar" ]; then 
-    echo "$compression is not zip and tar"
-    log_action "$compression is not zip and tar"
-    exit 1
-fi 
-
-timestamp=$(date '+%Y-%m-%d_%H-%M-%S')
-archived_file_name="backup-${timestamp}-${directory}.$compression"
-
-if [ $compression == "zip" ]; then 
-    # using zip command to zip directory
-    zip -r ${archived_file_name} ${directory}
-elif [ $compression == "tar" ]; then 
-    # using tar command to tar directory
-    tar cf ${archived_file_name} ${directory}
-fi 
-
-if [[ $? -ne 0 ]]; then 
-    log_action "Compression Failed for directory ${directory}"
-    exit 1
-fi         
-
-# writes action into log. 
-log_action "Compression for directory ${directory} Successful: ${archived_file_name}"
-
-# Check the given S3 location should be non empty 
-# Check the given S3 location should start with s3://
-if [ -n "$s3_location" ] && echo $s3_location | grep -q "^s3://" ; then 
-    echo "S3 Location $s3_location is provided in the arguments."
-    # Now write the archived file into S3. 
-    aws s3 cp ${archived_file_name} ${s3_location}
-fi
+s3_move $archived_file_name $s3_location 
